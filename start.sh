3254788214c9a3737d3aef7b02e87632f5f5d585
Victor Alexandrov <synclpz@gmail.com>
2020-08-06 02:36:55 +0300

    Finalize start script: change container name to appropriate add --rm

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ mkdir -p $CACHE_DIR
 
 PA_SOCKET="$(LC_ALL=C pactl info | grep 'Server String' | cut -f 3 -d' ')"
 
-docker run -d \
+docker run --rm -d \
     --env PULSE_SERVER=unix:/tmp/pulseaudio.socket \
     --env PULSE_COOKIE=/tmp/pulseaudio.cookie \
     --volume $PA_SOCKET:/tmp/pulseaudio.socket \
@@ -16,4 +16,4 @@ docker run -d \
     --user $(id -u):$(id -g) \
     --net host \
     --name spotifyd-pulseaudio \
-    spotifyd-pulseaudio
+    synclpz/spotifyd-pulseaudio
